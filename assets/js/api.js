7ac5308e0dcffad36f6b28b6a8357fad9b79b877
Luis Moraes <162765438+LuisMoraess@users.noreply.github.com>
2024-07-19 20:42:53 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/LuisMoraess/portfolio.github.io/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/LuisMoraess/portifolio.github.io/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
